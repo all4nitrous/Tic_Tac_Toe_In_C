57be974aa46a144e267f08d36a2331d75a1e5771
all4nitrous <all4nitrous@gmail.com>
2025-01-06 14:18:49 -0500

    Variables added for project

---
+++ main.c
@@ -0,0 +1,52 @@
+/* Tic Tac Toe written in C */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+/* Variables List */
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void computerMove();
+void checkWinner();
+void printWinner(char);
+
+/* Main Function */
+int main() {
+
+
+    return 0;
+}
+
+/* Bodies for variables */
+
+// Section for reseting the board
+void resetBoard() {
+
+}
+
+// Body for printing board
+void printBoard() {
+
+}
+
+//  Body in charge of spaces in the board
+int checkFreeSpaces() {
+
+}
+
+// Body for the computer to make its' moves
+void computerMove() {
+
+}
+
+// Body for checking the winner
+void checkWinner() {
+
+}
+
+// Prints the winner of the game
+void printWinner(char winner) {
+
+}
