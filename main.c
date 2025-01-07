4d1d22d3b7abfc93121f17969c67a7ddca57c6ac
all4nitrous <all4nitrous@gmail.com>
2025-01-07 13:36:18 -0500

    Check columns section finished

--- main.c
+++ main.c
@@ -109,10 +109,16 @@ void computerMove() {
 char checkWinner() {
     // Check rows
     for(int i = 0; i < 3; i++) {
-        if(board[i][0] == board[i][1] && board[i][0] ==board[i][2]) {
-            return board[i][0];
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+            return board[0][i];
         }
 
+    }
+    // Check Columns
+    for(int i = 0; i < 3; i++) {
+        if(board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+            return board[0][i];
+        }
     }
 }
 
